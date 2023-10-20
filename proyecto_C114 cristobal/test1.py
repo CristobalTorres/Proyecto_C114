9d748bd5d0f8e63674ab40c5f35e7e9c665e8df6
CristobalTorres <114037664+CristobalTorres@users.noreply.github.com>
2023-10-20 15:56:07 -0500

    Add files via upload

---
+++ proyecto_C114 cristobal/test1.py
@@ -0,0 +1,22 @@
+a=33
+b="33"
+
+if b>a :
+    print("B es mayour que A")
+elif b==a:
+    print("b es igual a a")
+
+
+
+##hay un error  dentro de la ejecucion ya que en 
+# la variable b es un elemento de texto y no uno numerico,
+# y debido a eso no puede realizar operaciones matematicas
+
+#codigo correcto
+a=33
+b=33
+
+if b>a :
+    print("B es mayour que A")
+elif b==a:
+    print("b es igual a a")
---
+++ proyecto_C114 cristobal/test2.py
@@ -0,0 +1,6 @@
+def my_function(name):
+    print(name+"refnes")
+
+my_function("Email")
+my_function("Tobias")
+my_function("linus")
